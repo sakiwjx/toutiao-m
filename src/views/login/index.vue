558c13eb9b714bf4c0cd66970710d19bb914d00d
wangjiaxin <2472393726@.com>
2022-08-13 11:56:30 +0800

    github使用

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,142 +1,39 @@
 <template>
   <div class="login-container">
     <!-- 导航栏 -->
-    <van-nav-bar class="page-nav-bar" title="标题" />
 
     <!-- 登录表单 -->
-    <van-form @submit="onSubmit" ref="loginForm">
-      <van-field
-        name="mobile"
-        placeholder="请输入手机号"
-        v-model="user.mobile"
-        :rules="userFormRules.mobile"
-        type="number"
-        maxlength="11"
-      >
-        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
-      </van-field>
-      <van-field
-        name="验证码"
-        placeholder="请输入验证码"
-        v-model="user.code"
-        :rules="userFormRules.code"
-        type="number"
-        maxlength="6"
-      >
-        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
-        <template #button>
-          <van-count-down
-            :time="time"
-            format="ss s"
-            v-if="isCountDown"
-            @finish="isCountDown = false"
-          />
-          <van-button
-            class="send-sms-btn"
-            size="small"
-            type="primary"
-            v-else
-            @click="sentBtn"
-            >发送验证码</van-button
-          >
-        </template>
-      </van-field>
-      <div class="login-btn-wrap" style="margin: 16px">
-        <van-button class="login-btn" block type="info" native-type="submit"
-          >提交</van-button
-        >
-      </div>
-    </van-form>
+
+    <!-- 插槽插入图标 -->
+
+    <!-- 插槽插入验证码按钮 -->
+
+    <!-- 倒计时 -->
+
+    <!-- 登录按钮 -->
   </div>
 </template>
 
 <script>
-import { login, sendSms } from '../../api/user'
 export default {
   name: 'Login',
   data () {
-    return {
-      user: {
-        mobile: '',
-        code: ''
-      },
-      userFormRules: {
-        mobile: [
-          { required: true, message: '请填写手机号' },
-          { pattern: /^1[3|5|7|8]\d{9}$/, message: '手机号格式错误' }
-        ],
-        code: [
-          { required: true, message: '请填写手机号' },
-          {
-            pattern: /^\d{6}$/,
-            message: '验证码格式错误'
-          }
-        ]
-      },
-      time: 1000 * 60,
-      isCountDown: false
-    }
+    return {}
   },
   methods: {
-    async onSubmit () {
-      this.$toast.loading({
-        message: '加载中...',
-        forbidClick: true,
-        duration: 0
-      })
-      try {
-        const res = await login(this.user)
-        this.$toast.success('登录成功')
-        console.log(res)
-      } catch (err) {
-        if (err.responese && err.responese.status === 400) {
-          this.$toast.fail('手机号或者验证码错误')
-        } else {
-          this.$toast.fail('登录失败')
-        }
-      }
-    },
-    async sentBtn () {
-      try {
-        await this.$refs.loginForm.validate('mobile')
-      } catch (err) {
-        return console.log(err)
-      }
-      this.isCountDown = true
-      try {
-        const res = await sendSms(this.user.mobile)
-        console.log(res)
-      } catch (err) {
-        console.log(err)
-      }
-    }
+    // 1登录点击事件
+    // 2 登录状态提示
+    // 3封装登录请求
+    // 发送登录请求
+    async onSubmit () {},
+    // 发送验证码点击事件
+    // 1通过表单的验证
+    // 将倒计时打开
+    // 封装发送验证码的请求
+    // 发送验证码处理
+    async sentBtn () {}
   }
 }
 </script>
 
-<style lang="less" scoped>
-.login-container {
-  .toutiao {
-    font-size: 37px;
-  }
-  .send-sms-btn {
-    // width: 152px;
-    height: 46px;
-    line-height: 46px;
-    background-color: #ededed;
-    font-size: 22px;
-    color: #666;
-  }
-  .login-btn-wrap {
-    padding: 53px 33px;
-    .login-btn {
-      background-color: #6db4fb;
-      border: none;
-    }
-  }
-}
-
-/deep/ .van-button--primary {
-  border-color: #1989fa !important;
-}
-</style>
+<style lang="less" scoped></style>
